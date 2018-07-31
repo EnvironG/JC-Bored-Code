ef3f586b6cfaa630333c9359c1e52b3e13e2c212
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 11:04:53 -0600

    Heroku Connect Test

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Bored Code</h1>
+        <h1>Heroku Cross Device Test</h1>
       </div>
     );
   }
